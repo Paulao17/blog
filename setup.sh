540384fe149d70dfdc1c5e32cfaddfc537028e93
Paul ALNET <paul.git@shimaore.net>
2020-12-19 18:22:59 +0100

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+mkdir themes     // Creates a "themes" Folder
+cd themes        // Points to the "themes" Folder
+git submodule add https://github.com/pacollins/hugo-future-imperfect-slim.git
+cd ..
