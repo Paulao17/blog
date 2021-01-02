eded18b04c9774bc18f38c0e06eb4980e6ffa9d0
Paul ALNET <paul@shimaore.net>
2021-01-02 12:06:37 +0100

    Add details to webhook script

--- webhook.sh
+++ webhook.sh
@@ -1,8 +1,11 @@
 # Update
+echo "1. Pulling from Github"
 git pull
 
 # Build
+echo "2. Building with Hugo"
 hugo
 
 # Copy to server directory
+echo "3. Copying static files to server static directory"
 cp -r public/* /var/www/blogslovetoparty # export BASEDIR=/var/www/examplecom/math
